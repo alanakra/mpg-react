603e90d63e9d1fb1d5b133e8a2161bfeea29ea97
Alan Akra <alanakra@hotmail.fr>
2023-06-29 12:32:58 +0200

    Use select tag instead datalist

--- src/components/ClubList.jsx
+++ src/components/ClubList.jsx
@@ -19,13 +19,12 @@ export default function ClubList() {
     return (
     <div>
         <h1 className="text-3xl text-center mb-4">Liste des clubs</h1>
-        <input type="text" list="select-club" name="select-club"/>
-        <datalist id="select-club">
+        <select id="select-club" onChange={submitClub}>
             {Object.keys(clubs).map((clubId) => {
                 const club = clubs[clubId];
-                return <option key={club.id}>{club.name["fr-FR"]}</option>;
+                return <option key={club.id} id={club.id}>{club.name["fr-FR"]}</option>;
             })}
-        </datalist>
+        </select>
     </div>
     );
 }
