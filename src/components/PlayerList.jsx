bed79d765c8b1ce2dc0c15e08ad957c533c27bed
Alan Akra <alanakra@hotmail.fr>
2023-07-09 12:04:58 +0200

    Filter Players by clubs

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -5,8 +5,24 @@ import { Link } from 'react-router-dom';
 export default function PlayerList() {
     const [filteredPlayers, setFilteredPlayers] = useState([]);
     const [filteredPosition, setFilteredPosition] = useState("");
+    const [selectedClub, setSelectedClub] = useState("");
+    const [clubs, setClubs] = useState([]);
     const [name, setName] = useState("");
     const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        async function fetchClubs() {
+        const response = await axios.get(
+            "https://api.mpg.football/api/data/championship-clubs"
+        );
+        const championshipClubs = response.data.championshipClubs;
+        setClubs(championshipClubs);
+        console.log(championshipClubs);
+    }
+
+    fetchClubs();
+    }, []);
+
     useEffect(() => {
         async function fetchPlayers() {
             const response = await axios.get(
@@ -15,13 +31,22 @@ export default function PlayerList() {
             const playersList = response.data.poolPlayers;
             setPlayers(playersList);
             setFilteredPlayers(playersList);
+            console.log(playersList);
         }
         fetchPlayers();
     }, []);
 
     useEffect(() => {
         filterPlayers();
-    }, [name, filteredPosition])
+    }, [name, filteredPosition, selectedClub])
+
+    function submitClub(e) {
+        e.preventDefault();
+        const selectedIndex = e.target.selectedIndex;
+        const selectedOption = e.target.options[selectedIndex].id;
+        console.log(selectedOption);
+        setSelectedClub(selectedOption);
+    }
 
     function handleChangePosition(e) {
         setFilteredPosition(e.target.value);
@@ -32,7 +57,14 @@ export default function PlayerList() {
     }
 
     const filterPlayers = () => {
+        console.log(clubs);
         let filteredPlayers = players;
+
+        if (selectedClub !== '') {
+            filteredPlayers = filteredPlayers.filter((player) => {
+                return player.clubId === selectedClub;
+            });
+        }
     
         if (name !== '') {
           filteredPlayers = filteredPlayers.filter((player) =>
@@ -50,16 +82,30 @@ export default function PlayerList() {
     return (
         <div>
             <h1 className="text-3xl text-center mt-8 mb-4">Liste des joueurs</h1>
-            <select id="positionFilter" value={filteredPosition} onChange={handleChangePosition}>
-                <option value="">Toutes</option>
-                <option value="10">Gardien - G</option>
-                <option value="20">Defenseur - D</option>
-                <option value="21">Lateral - L</option>
-                <option value="30">Milieu défensir - MD</option>
-                <option value="31">Milieu offensif - MO</option>
-                <option value="40">Attaquant - A</option>
-            </select>
             <div>
+                <label htmlFor="select-club" className="text-center mr-4 mb-4">Clubs : </label>
+                <select id="select-club" onChange={submitClub}>
+                    <option value="" id="">--Please choose an option--</option>
+                    {Object.keys(clubs).map((clubId) => {
+                        const club = clubs[clubId];
+                        return <option key={club.id} id={club.id}>{club.name["fr-FR"]}</option>;
+                    })}
+                </select>
+            </div>
+
+            <div>
+                <label htmlFor="positionfilter" className="mr-4">Position : </label>
+                <select id="positionFilter" value={filteredPosition} onChange={handleChangePosition}>
+                    <option value="">Toutes</option>
+                    <option value="10">Gardien - G</option>
+                    <option value="20">Defenseur - D</option>
+                    <option value="21">Lateral - L</option>
+                    <option value="30">Milieu défensir - MD</option>
+                    <option value="31">Milieu offensif - MO</option>
+                    <option value="40">Attaquant - A</option>
+                </select>
+            </div>
+            <div className="mb-2">
                 <label htmlFor="nameFilter" className="mr-4">Filtrer par nom : </label>
                 <input
                 type="text"
@@ -70,6 +116,7 @@ export default function PlayerList() {
                 />
             </div>
             <ul>
+                {filteredPlayers.length === 0 ? <p>Aucun joueur sélectionable</p> : ''}
                 {filteredPlayers.map((player) => (
                     <li key={player.id}>
                         <Link className="hover:underline" to={`/players/${player.id}?clubid=${player.clubId}&firstname=${player.firstName}&lastname=${player.lastName}`}>
